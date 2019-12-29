0747478c4d2cba1db9601bcfbdd64222d07983e2
Deepesh upadhayay <41473554+du0079@users.noreply.github.com>
2019-12-30 05:12:02 +0530

    commit 1

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+var bodyparser =require("body-parser"),
+    express    = require("express"),
+    expressSanitizer=require("express-sanitizer"),
+    methodOverride  = require("method-override"),
+    mongoose   = require("mongoose"),
+    app= express();
 
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(express.static("views"));
+
+// Route
+app.get("/",function(req,res){
+      res.render("index");
+});
+
+
+
+
+
+
+app.listen(3000,function(){
+ console.log("Tested OK");
+});
